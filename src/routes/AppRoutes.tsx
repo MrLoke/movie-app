f367556320702ec6d4571ce87a579c0ebd56a6ee
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-01 21:39:10 +0100

    Wrap app routes with layout

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,9 @@
 import Navbar from 'components/Navbar/Navbar'
+import Layout from 'layout/Layout'
 import FavouritePage from 'pages/FavouritePage/FavouritePage'
 import HomePage from 'pages/HomePage/HomePage'
 import MoviePage from 'pages/MoviePage/MoviePage'
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage'
 import WatchlistPage from 'pages/WatchlistPage/WatchlistPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import GlobalStyles from 'theme/GlobalStyles'
@@ -12,12 +14,15 @@ const AppRoutes = () => {
       <GlobalStyles />
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="movie/:name" element={<MoviePage />} />
-          <Route path="watchlist" element={<WatchlistPage />} />
-          <Route path="favourite" element={<FavouritePage />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="movie/:name" element={<MoviePage />} />
+            <Route path="watchlist" element={<WatchlistPage />} />
+            <Route path="favourite" element={<FavouritePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Layout>
       </Router>
     </>
   )
