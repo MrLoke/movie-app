5ae8ae61136ad83be0724be54c4fbd90fce01c02
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-03 18:22:13 +0100

    Add explanatory comment

--- src/utils/concatenateGenre.ts
+++ src/utils/concatenateGenre.ts
@@ -1,3 +1,4 @@
+// This function concatenate id's to api url like this: &with_genres=10,20,30
 const concatenateGenre = (selectedGenres: any[]) => {
   if (selectedGenres.length < 1) return ''
   const GenreIds = selectedGenres.map((g: { id: any }) => g.id)
