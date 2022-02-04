9f178f38a4412777d5125642c773c999ccc80d9e
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-04 21:35:27 +0100

    Refactor useFetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,89 +1,36 @@
-import { useEffect, useReducer, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
-interface State<T> {
-  data?: T
-  error?: Error
-}
-
-type Cache<T> = { [url: string]: T }
-
-// discriminated union type
-type Action<T> =
-  | { type: 'loading' }
-  | { type: 'fetched'; payload: T }
-  | { type: 'error'; payload: Error }
-
-const useFetch = <T = unknown>(
-  url?: string,
-  options?: RequestInit
-): State<T> => {
-  const cache = useRef<Cache<T>>({})
-
-  // Used to prevent state update if the component is unmounted
-  const cancelRequest = useRef<boolean>(false)
-
-  const initialState: State<T> = {
-    error: undefined,
-    data: undefined,
-  }
-
-  // Keep state logic separated
-  const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
-    switch (action.type) {
-      case 'loading':
-        return { ...initialState }
-      case 'fetched':
-        return { ...initialState, data: action.payload }
-      case 'error':
-        return { ...initialState, error: action.payload }
-      default:
-        return state
-    }
-  }
-
-  const [state, dispatch] = useReducer(fetchReducer, initialState)
+const useFetch = <T>(
+  url: string
+): {
+  data: T | null
+  error: Error | string
+} => {
+  const [data, setData] = useState<T | null>(null)
+  const [error, setError] = useState<Error | string>('')
 
   useEffect(() => {
-    // Do nothing if the url is not given
-    if (!url) return
+    const abortController = new AbortController()
 
-    const fetchData = async () => {
-      dispatch({ type: 'loading' })
-
-      // If a cache exists for this url, return it
-      if (cache.current[url]) {
-        dispatch({ type: 'fetched', payload: cache.current[url] })
-        return
-      }
-
+    const fetchData = async (): Promise<void> => {
       try {
-        const response = await fetch(url, options)
-        if (!response.ok) {
-          throw new Error(response.statusText)
-        }
-
-        const data = (await response.json()) as T
-        cache.current[url] = data
-        if (cancelRequest.current) return
-
-        dispatch({ type: 'fetched', payload: data })
+        const res = await fetch(url, { signal: abortController.signal })
+        const data = await res.json()
+        setData(data)
       } catch (error) {
-        if (cancelRequest.current) return
-
-        dispatch({ type: 'error', payload: error as Error })
+        if (error instanceof Error) {
+          setError(error.message)
+        }
       }
     }
+    fetchData()
 
-    void fetchData()
-
-    // Use the cleanup function for avoiding a possibly...
-    // ...state update after the component was unmounted
     return () => {
-      cancelRequest.current = true
+      abortController.abort()
     }
   }, [url])
 
-  return state
+  return { data, error }
 }
 
 export default useFetch
