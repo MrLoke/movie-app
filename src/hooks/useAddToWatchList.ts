4b7e5825baaa55820804e9ce8ab604888e3a02cd
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 00:09:10 +0100

    Create add to watch list movie hook

---
+++ src/hooks/useAddToWatchList.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { watchListState } from 'atoms/watchListAtom'
+
+const useAddToWatchList = (movie: any) => {
+  const [watchList, setWatchList] = useRecoilState(watchListState)
+  const [isWatchList, setIsWatchList] = useState(false)
+
+  useEffect(() => {
+    // Check if movie already exist in watch list array
+    setIsWatchList(watchList.findIndex((item) => item.id === movie.id) !== -1)
+  }, [watchList])
+
+  const addMovieToWatchList = (movie: any) => {
+    // If exist remove from array
+    if (isWatchList) {
+      setWatchList(watchList.filter((item) => item.id !== movie.id))
+    } else {
+      setWatchList([...watchList, movie])
+    }
+  }
+
+  return {
+    isWatchList,
+    addMovieToWatchList,
+  }
+}
+
+export default useAddToWatchList
