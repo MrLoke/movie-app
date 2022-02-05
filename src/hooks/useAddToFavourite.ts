76e7722b485cf75066409b3f083d424eef574bfb
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 00:08:23 +0100

    Create add to favourite movie list hook

---
+++ src/hooks/useAddToFavourite.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { favouriteState } from 'atoms/favouriteAtom'
+
+const useAddToFavourite = (movie: any) => {
+  const [favourite, setFavourite] = useRecoilState(favouriteState)
+  const [isFavourite, setIsFavourite] = useState(false)
+
+  useEffect(() => {
+    // Check if movie already exist in favourite array
+    setIsFavourite(favourite.findIndex((item) => item.id === movie.id) !== -1)
+  }, [favourite])
+
+  const addMovieToFavourite = (movie: any) => {
+    // If exist remove from array
+    if (isFavourite) {
+      setFavourite(favourite.filter((item) => item.id !== movie.id))
+    } else {
+      setFavourite([...favourite, movie])
+    }
+  }
+
+  return {
+    isFavourite,
+    addMovieToFavourite,
+  }
+}
+
+export default useAddToFavourite
