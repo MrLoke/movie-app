3f1d733ed57aceb03ec967b82494a61b05b71d86
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 22:42:05 +0100

    Add api url for backgound image

--- src/api/themoviedb.ts
+++ src/api/themoviedb.ts
@@ -1,5 +1,6 @@
 export const BASE_URL = 'https://api.themoviedb.org/3'
 export const SEARCH_URL = 'https://api.themoviedb.org/3/search'
 export const IMG_API = 'https://image.tmdb.org/t/p/w500'
+export const BG_IMG_API = 'https://image.tmdb.org/t/p/w1280'
 export const IMG_PLACEHOLDER =
   'https://www.movienewz.com/img/films/poster-holder.jpg'
