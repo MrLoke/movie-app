dfc46f19a07ad01bb1d25c01f570a5fa0e5d63c2
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 23:45:49 +0100

    Render watch list items

--- src/pages/WatchlistPage/WatchlistPage.tsx
+++ src/pages/WatchlistPage/WatchlistPage.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import Movie from 'components/Movie/Movie'
+import { watchListState } from 'atoms/watchListAtom'
+import { useRecoilValue } from 'recoil'
+import { Box, Flex, Heading } from 'theme-ui'
 
 const WatchlistPage = () => {
+  const watchList = useRecoilValue(watchListState)
   return (
-    <div>
-      <div></div>
-    </div>
+    <Box
+      sx={{
+        marginTop: '50px',
+      }}
+    >
+      <Heading
+        as="h2"
+        sx={{
+          textAlign: 'center',
+          marginBottom: '20px',
+          color: 'accent',
+        }}
+      >
+        Your watch list
+      </Heading>
+      <Flex
+        sx={{
+          flex: '0.75',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+        }}
+      >
+        {watchList.length > 0 ? (
+          watchList.map((item) => <Movie key={item.id} movie={item} />)
+        ) : (
+          <Heading sx={{ textAlign: 'center', marginTop: '20px' }}>
+            Watch list is empty.
+          </Heading>
+        )}
+      </Flex>
+    </Box>
   )
 }
 
