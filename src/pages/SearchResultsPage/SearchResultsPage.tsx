7fee9e656df4bb4f326be3897ea6f76cd8ac5b71
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 22:41:14 +0100

    Add pagination

--- src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -1,25 +1,51 @@
+import { useEffect, useState } from 'react'
 import Movie from 'components/Movie/Movie'
+import Pagination from 'components/Pagination/Pagination'
 import { useRecoilValue } from 'recoil'
 import { searchInputState } from 'atoms/searchInputAtom'
 import { SEARCH_URL } from 'api/themoviedb'
 import useFetch from 'hooks/useFetch'
 import { Flex, Text, Spinner } from 'theme-ui'
-import { MovieProps } from 'types/types'
+import { ResultsMovieTypes } from 'types/types'
 
 const SearchResultsPage = () => {
+  const [totalPages, setTotalPages] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
   const searchValue = useRecoilValue(searchInputState)
-  const url = `${SEARCH_URL}/movie/?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchValue}&page=1`
-  const { data, error } = useFetch<MovieProps>(url)
+  const url = `${SEARCH_URL}/movie/?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchValue}&page=${currentPage}`
+  const { data, error } = useFetch<ResultsMovieTypes>(url)
+
+  useEffect(() => {
+    if (data?.total_pages !== undefined) {
+      setTotalPages(data?.total_pages)
+    }
+  }, [data])
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
+    window.scrollTo(0, 0)
+  }
 
   return (
     <Flex
       sx={{
-        flexWrap: 'wrap',
-        justifyContent: 'center',
+        marginTop: '50px',
+        flexDirection: 'column',
       }}
     >
       {data ? (
-        data.results.map((item) => <Movie key={item.id} movie={item} />)
+        <>
+          <Flex sx={{ flexWrap: 'wrap', justifyContent: 'center' }}>
+            {data.results.map((item) => (
+              <Movie key={item.id} movie={item} />
+            ))}
+          </Flex>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            handlePageChange={handlePageChange}
+          />
+        </>
       ) : (
         <Spinner />
       )}
