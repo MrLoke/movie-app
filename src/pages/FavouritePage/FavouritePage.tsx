e4c84d6015a5fb7c23a2166750e1ccc31e7b6b5e
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 23:46:42 +0100

    Render favourite items

--- src/pages/FavouritePage/FavouritePage.tsx
+++ src/pages/FavouritePage/FavouritePage.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import Movie from 'components/Movie/Movie'
+import { favouriteState } from 'atoms/favouriteAtom'
+import { useRecoilValue } from 'recoil'
+import { Box, Flex, Heading } from 'theme-ui'
 
 const FavouritePage = () => {
+  const favourite = useRecoilValue(favouriteState)
   return (
-    <div>
-      <div></div>
-    </div>
+    <Box
+      sx={{
+        marginTop: '50px',
+      }}
+    >
+      <Heading
+        as="h2"
+        sx={{
+          textAlign: 'center',
+          marginBottom: '20px',
+          color: 'accent',
+        }}
+      >
+        Your favourite movies
+      </Heading>
+      <Flex
+        sx={{
+          flex: '0.75',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+        }}
+      >
+        {favourite.length > 0 ? (
+          favourite.map((item) => <Movie key={item.id} movie={item} />)
+        ) : (
+          <Heading sx={{ textAlign: 'center', marginTop: '20px' }}>
+            Favourite movies list is empty.
+          </Heading>
+        )}
+      </Flex>
+    </Box>
   )
 }
 
