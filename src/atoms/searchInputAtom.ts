fb8fae4dbf59fa93afd6aaff782af75ed2618843
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 22:36:00 +0100

    Add persistence search input value in local storage

--- src/atoms/searchInputAtom.ts
+++ src/atoms/searchInputAtom.ts
@@ -1,6 +1,26 @@
-import { atom } from 'recoil'
+import { atom, AtomEffect, DefaultValue } from 'recoil'
+
+// Need to persist search value after refresh page on search route
+
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue))
+    }
+
+    onSet((newValue) => {
+      if (newValue instanceof DefaultValue) {
+        localStorage.removeItem(key)
+      } else {
+        localStorage.setItem(key, JSON.stringify(newValue))
+      }
+    })
+  }
 
 export const searchInputState = atom({
   key: 'searchInputState',
   default: '',
+  effects: [localStorageEffect('search input')],
 })
