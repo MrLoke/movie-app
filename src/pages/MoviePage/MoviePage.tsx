f6ea94abe6c2b602638f04a9888f463c34e8c0ae
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-06 22:40:11 +0100

    Fetch detail movie based on id

--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import DetailMovie from 'components/DetailMovie/DetailMovie'
+import { Box, Flex, Text, Spinner } from 'theme-ui'
+import useFetch from 'hooks/useFetch'
+import { CustomizedState, DetailMovieTypes } from 'types/types'
+import { useLocation } from 'react-router-dom'
+import { BASE_URL } from 'api/themoviedb'
 
 const MoviePage = () => {
+  const location = useLocation()
+  const state = location.state as CustomizedState
+  const { id } = state
+  const url = `${BASE_URL}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+  const { data, error } = useFetch<DetailMovieTypes>(url)
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Flex sx={{ marginTop: '50px' }}>
+      {data ? <DetailMovie movie={data} /> : <Spinner />}
+      <Box>{error ? <Text>{error}</Text> : null}</Box>
+    </Flex>
   )
 }
 
