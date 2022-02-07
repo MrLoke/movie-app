676e9b111ae1db2af878cb20a958b3b35a857201
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-02-07 03:51:14 +0100

    Add beforeEach hook

--- src/__tests__/SearchInput.test.tsx
+++ src/__tests__/SearchInput.test.tsx
@@ -3,26 +3,22 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { BrowserRouter } from 'react-router-dom'
 
+beforeEach(() => {
+  render(
+    <BrowserRouter>
+      <SearchInput />
+    </BrowserRouter>
+  )
+})
+
 describe('<SearchInput />', () => {
   it('render search input', () => {
-    render(
-      <BrowserRouter>
-        <SearchInput />
-      </BrowserRouter>
-    )
-
     const inputEl = screen.getByPlaceholderText('Search movie')
 
     expect(inputEl).toHaveAttribute('type', 'text')
   })
 
   it('change input value', async () => {
-    render(
-      <BrowserRouter>
-        <SearchInput />
-      </BrowserRouter>
-    )
-
     const inputEl = screen.getByPlaceholderText('Search movie')
     userEvent.type(inputEl, 'Avengers')
 
